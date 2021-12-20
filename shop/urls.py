bda3a1e30f5d0a29185e84793f08b52dade5bfd0
Melodycadeance <Melodycadeance>
2021-12-20 17:44:25 +0500

    Added a model, a view and a template for the site

---
+++ shop/urls.py
@@ -0,0 +1,6 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.items_list, name='items_list'),
+]
--- shop/views.py
+++ shop/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def items_list(request):
+    return render(request, 'shop/items_list.html', {})
